524ca5c9aedb2f351b55445791f3efa65de46ba6
Samuel Castaño Mira <scastanomi@unal.edu.co>
2023-09-21 15:02:05 -0500

    d

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,18 +1,17 @@
-class Zona:
-    def __init__(self,nombre="",zoo=None):
-        self._nombre=nombre
-        self._zoo= zoo
-        self._animales= []
+class Zona():
+    def __init__(self, nombre="",zoo=None):
+        self._nombre = nombre
+        self._zoo = zoo
+        self._animales=[]
         
-    def agregarAnimales(self,NuevoAnimal):
-        self.animales.append(NuevoAnimal)
+    def agregarAnimales(self,Newanimal):
+        self._animales.append(Newanimal)
         
     def cantidadAnimales(self):
         return len(self._animales)
-    
-    def getNombre(self):
-        return self._nombre
-    def getZoo(self):
-        return self._zoo
-    def getAnimales(self):
+    def getAnimales(self): 
         return self._animales
+    def getZoo(self): 
+        return self._zoo
+    def getNombre(self): 
+        return self._nombre
